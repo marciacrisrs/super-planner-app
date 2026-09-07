1080ed9c5438296ecebba5f7070d86d2ad436482
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-09-07 17:59:10 -0300

    feat(operations): request notification permission

--- app/src/main/java/com/gpsdavida/app/ui/MainActivity.kt
+++ app/src/main/java/com/gpsdavida/app/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.gpsdavida.app.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.gpsdavida.app.ui.navigation.GpsNavHost
 import com.gpsdavida.app.ui.theme.GpsDaVidaTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,10 +18,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        requestNotificationsPermission()
         setContent {
-            GpsDaVidaTheme {
-                GpsNavHost()
-            }
+            GpsDaVidaTheme { GpsNavHost() }
+        }
+    }
+
+    private fun requestNotificationsPermission() {
+        if (Build.VERSION.SDK_INT >= 33 && ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1001)
         }
     }
 }
