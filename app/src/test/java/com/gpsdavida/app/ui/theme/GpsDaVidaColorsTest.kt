f5711d08debcaa5cf6a4cfe6efc48c6c30c3cc26
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-09-14 13:38:39 -0300

    test(theme): assert official palette alpha values

--- app/src/test/java/com/gpsdavida/app/ui/theme/GpsDaVidaColorsTest.kt
+++ app/src/test/java/com/gpsdavida/app/ui/theme/GpsDaVidaColorsTest.kt
@@ -8,20 +8,20 @@ class SuperPlannerColorsTest {
     @Test
     fun officialPaletteKeepsWarmPlannerCanvas() {
         assertArgbEquals(0xFFFCF6F2.toInt(), SuperPlannerColors.Canvas.toArgb())
-        assertArgbEquals(0xFFFFFBF8.toInt(), SuperPlannerColors.Surface.toArgb())
+        assertArgbEquals(0xE6FFFBF8.toInt(), SuperPlannerColors.Surface.toArgb())
     }
 
     @Test
     fun officialPaletteKeepsTerracottaAsPrimaryAccent() {
-        assertArgbEquals(0xFFC56245.toInt(), SuperPlannerColors.Terracotta.toArgb())
-        assertArgbEquals(0xFFF5CFC5.toInt(), SuperPlannerColors.TerracottaSoft.toArgb())
+        assertArgbEquals(0xE6C56245.toInt(), SuperPlannerColors.Terracotta.toArgb())
+        assertArgbEquals(0xCCF5CFC5.toInt(), SuperPlannerColors.TerracottaSoft.toArgb())
     }
 
     @Test
     fun officialPaletteContainsSecondaryOrganicAccents() {
-        assertArgbEquals(0xFFE69A8F.toInt(), SuperPlannerColors.Rose.toArgb())
-        assertArgbEquals(0xFFA7B89F.toInt(), SuperPlannerColors.Sage.toArgb())
-        assertArgbEquals(0xFF8DA1B8.toInt(), SuperPlannerColors.BlueGray.toArgb())
+        assertArgbEquals(0xE6E69A8F.toInt(), SuperPlannerColors.Rose.toArgb())
+        assertArgbEquals(0xE6A7B89F.toInt(), SuperPlannerColors.Sage.toArgb())
+        assertArgbEquals(0xE68DA1B8.toInt(), SuperPlannerColors.BlueGray.toArgb())
     }
 
     private fun assertArgbEquals(expected: Int, actual: Int) {
